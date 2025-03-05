39826aab7bb3bd32adcfe73f8f9ecf4805ad9dc8
Kateryna Bilokur <kotiku@gmail.com>
2025-03-05 22:05:19 +1100

    Fixed a bug with connection Redis string

--- app/_lib/redis.js
+++ app/_lib/redis.js
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redis = createClient(process.env.REDIS_URL);
+const redis = createClient({ url: process.env.REDIS_URL });
 
 redis.on("error", (err) => console.error("Redis Client Error", err));
 
