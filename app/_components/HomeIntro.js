f4abbb8ffb8143ead63a0497074b36e40cdfb50c
Kateryna Bilokur <kotiku@gmail.com>
2025-03-06 21:34:02 +1100

    al-3 Intro page cleanup more

--- app/_components/HomeIntro.js
+++ app/_components/HomeIntro.js
@@ -6,14 +6,6 @@ export default function HomeIntro() {
   return (
     <div className="min-h-screen bg-gradient-to-r from-primary_rose-200 via-primary_blue-200 to-primary_tur-200 bg-[length:200%_200%] animate-gradient-bg">
       <div className="flex w-80 sm:min-w-160 mx-auto flex-col gap-4 items-center justify-center">
-        {/* <Image
-          src={welcomePic}
-          placeholder="blur"
-          quality={100}
-          loading="lazy"
-          alt="Intro Audio List app picture"
-          className="w-72 sm:w-80 h-72 sm:h-80 rounded-3xl border border-white mt-4 mb-3 sm:mt-8 sm:mb-4"
-        /> */}
         <div className="relative w-72 sm:w-80 h-72 sm:h-80 overflow-hidden rounded-3xl border border-white mt-4 mb-3 sm:mt-8 sm:mb-4">
           <Image
             src={welcomePic}
@@ -31,7 +23,6 @@ export default function HomeIntro() {
         <h3 className="mb-2 sm:mb-4 text-xl sm:text-2xl font-medium text-zinc-600">
           Manage your shopping
         </h3>
-        {/* <hr className="w-48 h-1 mx-auto bg-primary_tur-300 border-0 rounded-sm md:my-10"></hr> */}
         <p className="text-base sm:text-lg text-zinc-500 italic mb-3">
           Planning and managing shopping was never that easy before! Add
           shopping items with just your voice. Organize lists effortlessly, and
