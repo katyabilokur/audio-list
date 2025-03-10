4bfe4e93f428d07725ff7f9da1982084eba615e7
Kateryna Bilokur <kotiku@gmail.com>
2025-03-10 14:14:34 +1100

    Styling Login page

--- app/_components/SignInButton.js
+++ app/_components/SignInButton.js
@@ -4,14 +4,30 @@ import { signInAction } from "../_lib/actions";
 function SignInButton() {
   return (
     <form action={signInAction}>
-      <button>
+      <button className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-700 rounded-2xl bg-zinc-100 hover:text-gray-900 hover:bg-gray-300">
         <Image
+          className="me-3"
           src="https://authjs.dev/img/providers/google.svg"
           alt="Google logo"
           height="24"
           width="24"
         />
-        <span>Continue with Google</span>
+        <span className="w-full text-sm">Continue with Google</span>
+        <svg
+          className="w-4 h-4 ms-2 rtl:rotate-180"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 10"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 5h12m0 0L9 1m4 4L9 9"
+          />
+        </svg>
       </button>
     </form>
   );
--- app/login/page.js
+++ app/login/page.js
@@ -1,13 +1,29 @@
 import SignInButton from "../_components/SignInButton";
+import { ShoppingBagIcon, MicrophoneIcon } from "@heroicons/react/24/outline";
 export const metadata = {
   title: "Login",
 };
 
 export default function Page() {
   return (
-    <div>
-      <h2>Sign in to access your account</h2>
-      <SignInButton />
+    <div className="flex justify-center items-start pt-24 min-h-screen bg-gradient-to-r from-primary_rose-200 via-primary_blue-200 to-primary_tur-200 bg-[length:200%_200%] animate-gradient-bg">
+      <div className="text-center p-8 sm:p-16 pb-16 sm:pb-20 flex flex-col gap-8 w-80 sm:w-96 rounded-lg bg-white border border-primary_purple-600">
+        <div className="mx-auto relative w-16 h-16">
+          <ShoppingBagIcon className="absolute inset-0 h-16 w-16 text-zinc-700 opacity-80" />
+          <MicrophoneIcon className="absolute inset-0 h-12 w-12 text-zinc-700 opacity-90 translate-x-3 translate-y-3" />
+        </div>
+
+        <h2 className="mb-2 sm:mb-4 text-3xl sm:text-4xl font-extrabold text-zinc-600">
+          Welcome!
+        </h2>
+        <p className="text-base sm:text-xl text-zinc-500 mb-3">
+          Sign in to start using{" "}
+          <span className="leading-none tracking-tight font-medium text-transparent bg-clip-text bg-gradient-to-r to-primary_tur-700 from-primary_purple-800">
+            Audio List
+          </span>
+        </p>
+        <SignInButton />
+      </div>
     </div>
   );
 }
