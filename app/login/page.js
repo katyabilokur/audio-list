132d1f6067a4380472cdb471d3f050ce703bfb85
Kateryna Bilokur <kotiku@gmail.com>
2025-03-11 12:12:16 +1100

    Styling Login page: logo. Added favicon icon

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,6 @@
 import SignInButton from "../_components/SignInButton";
-import { ShoppingBagIcon, MicrophoneIcon } from "@heroicons/react/24/outline";
+import Logo from "../_components/visual/Logo";
+
 export const metadata = {
   title: "Login",
 };
@@ -7,10 +8,12 @@ export const metadata = {
 export default function Page() {
   return (
     <div className="flex justify-center items-start pt-24 min-h-screen bg-gradient-to-r from-primary_rose-200 via-primary_blue-200 to-primary_tur-200 bg-[length:200%_200%] animate-gradient-bg">
-      <div className="text-center p-8 sm:p-16 pb-16 sm:pb-20 flex flex-col gap-8 w-80 sm:w-96 rounded-lg bg-white border border-primary_purple-600">
+      <div className="text-center p-8 sm:p-16 pb-16 sm:pb-20 flex flex-col gap-8 w-80 sm:w-96 rounded-2xl bg-white border border-primary_purple-800">
         <div className="mx-auto relative w-16 h-16">
-          <ShoppingBagIcon className="absolute inset-0 h-16 w-16 text-zinc-700 opacity-80" />
-          <MicrophoneIcon className="absolute inset-0 h-12 w-12 text-zinc-700 opacity-90 translate-x-3 translate-y-3" />
+          <Logo
+            className="absolute inset-0 h-16 w-16"
+            colour="text-primary_purple-700"
+          />
         </div>
 
         <h2 className="mb-2 sm:mb-4 text-3xl sm:text-4xl font-extrabold text-zinc-600">
